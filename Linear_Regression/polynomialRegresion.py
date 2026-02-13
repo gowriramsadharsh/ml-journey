8a31c7ffd021b69994727d0ad52c368aedd331b8
gowriramsadharsh <adharshgowrirams@gmail.com>
2026-02-13 23:36:16 +0530

    Polynomial regression code setup

---
+++ Linear_Regression/polynomialRegresion.py
@@ -0,0 +1,13 @@
+##
+import numpy as np
+
+np.random.seed(0)
+
+n = 10  # number of samples
+
+x1 = np.random.uniform(0, 10, n) #Distance random 
+x2 = np.random.uniform(0, 5, n)  #Prep time
+x3 = np.random.uniform(1, 3, n)  #Traffic 
+print (x1)
+print(x2)
+print(x3)
